8a572967d819610df81cfdae7ee9a047ca62394f
NicoRosane <100003788+Nico2R91@users.noreply.github.com>
2023-11-01 12:12:51 -0300

    cambios en toda la pagina

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+// Obtén el elemento del navbar por su ID
+var navbar = document.getElementById("navbar");
+
+// Agrega un evento para cambiar la opacidad al pasar el cursor por encima
+navbar.addEventListener("mouseover", function() {
+    navbar.style.backgroundColor = "rgba(255, 255, 255, 1)"; // Hacer el navbar completamente visible
+});
+
+navbar.addEventListener("mouseout", function() {
+    navbar.style.backgroundColor = "rgba(255, 255, 255, 0.7)"; // Restaurar la opacidad al 70%
+});
+
+
+
 document.getElementById("Cotiza_tu_seguro").addEventListener("change", function () {
   var selectedOption = this.value;
   var valorSeguroElement = document.getElementById("valorSeguro");
